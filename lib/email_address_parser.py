91bc22da93da834c490902893d3af0bbe8eea1b1
Nicole Listerfelt <nlisterfelt@gmail.com>
2023-09-17 20:14:49 -0500

    email sort and filter

--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -1,1 +1,11 @@
-# your code goes here!
+import re
+
+class EmailAddressParser:
+    def __init__(self, emails):
+        self.emails = emails
+
+    def parse(self):
+        patternSplit = re.compile(r'[ \,]+')
+        patternFilter = re.compile(r'[a-z\.]+\@\w+\.com')
+        emailList = sorted(re.split(patternSplit, self.emails))
+        return [x for x in emailList if re.match(patternFilter, x)]
